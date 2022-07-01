67a040f71ad3256c4a962f6dfec7ba0947855340
Rafael Sangali <rafaelsangali@outlook.com>
2022-07-01 01:16:53 -0300

    re-building navbar

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,14 @@
 import { ParticlesHome } from "../components/HomeComponents/ParticlesHome";
 import { Sidebar } from "../components/Sidebar/Sidebar";
+import { Testing } from "../components/Testing";
 
 
 export function Home() {
   return (
     <main className="md:flex">
-      <Sidebar />
-      <ParticlesHome />
+      {/* <Sidebar />
+      <ParticlesHome /> */}
+      <Testing />
     </main>
   )
 }
